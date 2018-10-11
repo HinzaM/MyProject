28f6ca620e5ba033b9466073024ba3cf3a761204
t00203592 <nooramin261126@gmail.com>
2018-10-11 14:27:49 +0100

    Done

--- 3. Problem Sets/Set2/src/Fraction/Fraction.java
+++ 3. Problem Sets/Set2/src/Fraction/Fraction.java
@@ -1,4 +1,78 @@
 package Fraction;
 
 public class Fraction {
+
+    //private variables
+    private int numerator, denominator;
+
+
+    //no arg cons...
+    public Fraction()
+    {
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+
+    //arg cons...
+    public Fraction(int numerator, int denominator)
+    {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+
+    //accessor
+    public void setNum(int numerator) { this.numerator = numerator; }
+    public void setDen(int denominator) { this.denominator = denominator; }
+
+
+    //mutator
+    public int getNum() { return this.numerator; }
+    public int getDen() { return this.denominator; }
+
+
+    //toString method
+    public String toString() {
+           return getNum() + "/" + getDen();
+    }
+
+
+    //calculations
+    public Fraction add(Fraction x)
+    {
+         Fraction sum = new Fraction();
+         sum.numerator = numerator * x.denominator + x.numerator * denominator;
+         sum.denominator = denominator * x.denominator;
+         return sum;
+    }
+
+    public Fraction subtract(Fraction x) {
+        Fraction sum = new Fraction();
+        sum.numerator = numerator * x.denominator - x.numerator * denominator;
+        sum.denominator = denominator * x.denominator;
+        return sum;
+    }
+
+    public Fraction multiply(Fraction x) {
+        Fraction sum = new Fraction();
+        sum.numerator = numerator * x.numerator;
+        sum.denominator = denominator * x.denominator;
+        return sum;
+    }
+
+    public Fraction divide(Fraction x) {
+        Fraction sum = new Fraction();
+        sum.numerator = numerator * x.denominator;
+        sum.denominator = denominator * x.numerator;
+        return sum;
+    }
+
+
+
+
+
 }
+
+
+
--- 3. Problem Sets/Set2/src/Fraction/FractionTest.java
+++ 3. Problem Sets/Set2/src/Fraction/FractionTest.java
@@ -1,4 +1,20 @@
 package Fraction;
 
 public class FractionTest {
+    public static void main(String[] args) {
+
+
+        Fraction fraction1 = new Fraction(15,25);
+        Fraction fraction2 = new Fraction(10,20);
+        Fraction fraction3 = new Fraction();
+
+
+        System.out.println("\nTesting the no-argument constructor.... " + fraction3.toString());
+        System.out.println("Testing the 2-argument constructor.... " + fraction1.toString());
+
+        System.out.println("\nValue of " + fraction1.toString() + " + " + fraction2.toString() + " is " + fraction1.add(fraction2));
+        System.out.println("\nValue of " + fraction1.toString() + " - " + fraction2.toString() + " is " + fraction1.subtract(fraction2));
+        System.out.println("\nValue of " + fraction1.toString() + " * " + fraction2.toString() + " is " + fraction1.multiply(fraction2));
+        System.out.println("\nValue of " + fraction1.toString() + " / " + fraction2.toString() + " is " + fraction1.divide(fraction2));
+    }
 }
